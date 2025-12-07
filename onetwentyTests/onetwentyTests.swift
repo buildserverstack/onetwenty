c162631626e662df58c61f1f164769cd86caa2e5
sujay <157316320+buildserverstack@users.noreply.github.com>
2025-12-07 14:54:31 -0600

    Initial Commit

---
+++ onetwentyTests/onetwentyTests.swift
@@ -0,0 +1,17 @@
+//
+//  onetwentyTests.swift
+//  onetwentyTests
+//
+//  Created by sujay Chandra on 12/7/25.
+//
+
+import Testing
+@testable import onetwenty
+
+struct onetwentyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
